17281ae447ea11a318e7d0876a45aae7fc5ec8cb
Govind Nair <govind83nair@gmail.com>
2022-01-20 14:53:55 -0500

    the functions file was not added, rectified

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -1,6 +1,25 @@
 import wikipedia
+from Bio import Entrez
 
+# To contact NCBI without any issues
+#Entrez.api_key = "MyAPIkey"
+Entrez.email = "govind83nair@gmail.com"
+
+# Getting the Entrez DBs here so that this operation is nor repeated
+#ehan = Entrez.einfo()
+#entrez_dbs = ehan.read(ehan)
 
 def search_on_wikipedia(query):
     results = wikipedia.summary(query, sentences=2)
+    #references = wikipedia.WikipediaPage.references(query)
     return(results)
+
+def search_entrez(query):
+    ehan = Entrez.einfo()
+    entrez_dbs = ehan.read(ehan)
+    db_counts = {}
+    for dbn in entrez_dbs(['DbList']):
+        entrez_handle = Entrez.esearch(db= dbn,term=query,retmax="40")
+        entrez_record = Entrez.read(entrez_handle)
+        db_counts[dbn] = entrez_record["Count"]
+    return(db_counts)
